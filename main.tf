8526952e8e946b1ff0281cb382c06f75a2c0c9b9
loannguyent5 <loannguyent5@nashtechglobal.com>
2024-12-30 13:34:22 +0700

    first commit

---
+++ main.tf
@@ -0,0 +1,70 @@
+# Provider configuration with subscription and tenant details
+provider "azurerm" {
+  features {}
+
+  # Specify the subscription ID
+  subscription_id = "daa58596-3010-4dd4-b81f-0cc9be025d87"
+  
+  # Optional: Specify tenant ID if needed (can be skipped if using default tenant)
+  tenant_id       = "f4308c54-0208-43d3-afad-1f8df2f678b7"
+}
+
+# Create Resource Group
+resource "azurerm_resource_group" "rg" {
+  name     = "example-rg"
+  location = "East US"
+}
+
+# Create Virtual Network
+resource "azurerm_virtual_network" "vnet" {
+  name                = "example-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+# Create Subnet
+resource "azurerm_subnet" "subnet" {
+  name                 = "example-subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+# Create Azure Container Registry (ACR)
+resource "azurerm_container_registry" "acr" {
+  name                = "exampleacr1234"  # Must be globally unique
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  sku                 = "Standard"
+  admin_enabled       = true
+}
+
+# Create Azure Kubernetes Service (AKS)
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = "example-aks"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  dns_prefix          = "exampleaks"
+
+  # Configure the default node pool
+  default_node_pool {
+    name       = "default"
+    node_count = 2
+    vm_size    = "Standard_DS2_v2"
+  }
+
+  # Use System Assigned Identity
+  identity {
+    type = "SystemAssigned"
+  }
+
+  # Configure Network Profile
+  network_profile {
+    network_plugin = "azure"
+    dns_service_ip = "10.0.2.10"
+    service_cidr   = "10.0.2.0/24"
+  }
+
+  depends_on = [azurerm_subnet.subnet]
+}
